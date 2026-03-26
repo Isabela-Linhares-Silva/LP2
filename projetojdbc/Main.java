88f08bef31e4afe896c5657b503d74b7fa838647
Isabela-Linhares-SIlva <isabelalinharessilva@gmail.com>
2026-03-26 15:57:15 -0300

    mudando o da main para uma função específica

--- projetojdbc/Main.java
+++ projetojdbc/Main.java
@@ -6,23 +6,28 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) { 
-    Connection connection = null;//conection o com anco
-    Statement st = null;// execulta o sql
-    ResultSet rs = null;//guarda o resultado do select
+        exibir();
 
-    try {
-        connection = DB.getConnection();
-        st = connection.createStatement();
-        rs = st.executeQuery("select nome,cpf from aluno");
-        while(rs.next()){
-        System.out.println(rs.getString("nome") +
-        rs.getString("cpf"));
-    }
-    } catch (SQLException e) {
-        throw new RuntimeException(e);
+
     }
 
+    public static void exibir() {
+        Connection connection = null;//conection o com anco
+        Statement st = null;// execulta o sql
+        ResultSet rs = null;//guarda o resultado do select
 
+        try {
+            connection = DB.getConnection();
+            st = connection.createStatement();
+            rs = st.executeQuery("select nome,cpf from aluno");
+            while(rs.next()){
+            System.out.println(rs.getString("nome") +
+            rs.getString("cpf"));
+        }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        
     }
 }
 
