1653bc10bde337beb876cb23ba4e08581d0ad84b
Isabela-Linhares-SIlva <isabelalinharessilva@gmail.com>
2026-03-26 16:44:48 -0300

    criação do inserir, deletar e atualizar

--- projetojdbc/Main.java
+++ projetojdbc/Main.java
@@ -1,27 +1,75 @@
 package projetojdbc;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
+    static Connection connection = null;//conection o com anco
+    static Statement st = null;// execulta o sql// utilizado para visualizar
+    static ResultSet rs = null;//guarda o resultado do select
+    static PreparedStatement pst = null;// quando quer mexer no banco, fazer injeçoes
     public static void main(String[] args) { 
+        //inserir();
+        //deletar();
+        atualizar();
         exibir();
+    
+    }
 
+    public static void inserir(){
+        connection = DB.getConnection();
+        try {
+            pst = connection.prepareStatement("insert into aluno(nome,cpf,dataNascimento) values (?,?,?)");
+            pst.setString(1, "Tonia");// esse 1º numero indica a posição da "?", ou seja o 1 quer dizer o nome
+            pst.setString(2, "333.333.333-33");
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+            try {
+                pst.setDate(3, new Date(sdf.parse("10-10-2010").getTime()));// getTime tranforma a data em milisegundos
+            } catch (ParseException e) {
+               throw new RuntimeException(e);
+            }
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public static void deletar() {
+        connection = DB.getConnection();
+
+        try {
+            pst = connection.prepareStatement("delete from aluno where matricula= ?");
+            pst.setInt(1, 4);
+            pst.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void atualizar() {
+        connection = DB.getConnection();
+        try {
+            pst = connection.prepareStatement("update aluno set nome = ? where matricula =?");
+            pst.setString(1, "Karla");
+            pst.setInt(2, 2);
+            pst.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void exibir() {
-        Connection connection = null;//conection o com anco
-        Statement st = null;// execulta o sql
-        ResultSet rs = null;//guarda o resultado do select
-
         try {
             connection = DB.getConnection();
             st = connection.createStatement();
             rs = st.executeQuery("select nome,cpf from aluno");
             while(rs.next()){
-            System.out.println(rs.getString("nome") +
+            System.out.println(rs.getString("nome") + " "+
             rs.getString("cpf"));
         }
         } catch (SQLException e) {
@@ -29,6 +77,7 @@ public class Main {
         }
         
     }
+
 }
 
 
